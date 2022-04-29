0b86aec20484b369216530f600abe5947a678a5f
VictoriaKrumm <www.victoria@bk.ru>
2022-04-29 19:08:08 +0700

    Lab 3

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -3,17 +3,16 @@ package com.bignerdranch.android.photogallery.api
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface FlickrApi {
-    @GET(
-        "services/rest/? method=flickr.interestingness.getList" +
-                "&api_key=5ee71de6834b9c46141d39bc4dca0330" +
-                "&format=json" +
-                "&nojsoncallback=1" +
-                "&extras=url_s"
-    )
-    fun fetchPhotos(): Call<FlickrResponse>
+    @GET("services/rest?method=flickr.interestingness.getList")
+
+        fun fetchPhotos(): Call<FlickrResponse>
     @GET
     fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+    @GET("services/rest?method=flickr.photos.search")
+        fun searchPhotos(@Query("text") query: String): Call<FlickrResponse>
+
 }
